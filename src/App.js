d80e8e09076da39c14a9a73ce401ccd940a72a67
hoanganndev <hoangann.dev@gmail.com>
2022-07-04 20:13:22 +0700

    setup redux for project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Header from "./components/Header/Header";
+import { increaseCounter, decreaseCounter } from "./redux/action/couterAction";
 const App = () => {
+    const dispatch = useDispatch();
+    const count = useSelector(state => state.counter.count);
     return (
         <div className="App">
             <Header />
+            <div>Count: {count}</div>
+
+            <button onClick={() => dispatch(increaseCounter())}>
+                Increase Count
+            </button>
+
+            <button onClick={() => dispatch(decreaseCounter())}>
+                Decrease Count
+            </button>
         </div>
     );
 };
